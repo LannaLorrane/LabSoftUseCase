b07e66876b51e22116cfccc60e135fab04469ba2
LannaLorrane <lanna.lorrane18@gmail.com>
2026-08-17 09:21:10 -0300

    Commit- Funcionario e Incidente

---
+++ SLNTask/DataBase/Script02-Incidente.sql
@@ -0,0 +1,11 @@
+USE dbTasks;
+GO
+
+CREATE TABLE Incidente (
+    Codigo INT IDENTITY(1,1) PRIMARY KEY,
+    DescricaoProblema VARCHAR(250) NOT NULL,
+    DataIncidente DATETIME NOT NULL,
+    Solucao VARCHAR(250) NULL,
+    Resolvido VARCHAR(3) NOT NULL -- 'sim' ou 'nao'
+);
+GO
